5933f3daaf8bff9f5dcd787d435449fd378fcef3
John Pavley <john@pavley.com>
2014-06-13 22:12:55 -0400

    converted tabs to spaces!

--- dungeonators.js
+++ dungeonators.js
@@ -5,82 +5,82 @@
 var assert = require('assert');
 
 var PLAYER_STANCES = {
-	"ready"     : 0,
-	"attacking" : 1,
-	"defending" : 2
+  "ready"     : 0,
+  "attacking" : 1,
+  "defending" : 2
 };
 
 var STD_HEALTH = 1000000,
-		STD_SHIELD = 1000000;
+    STD_SHIELD = 1000000;
 
 // Character Class - Represents a playable character in the game
 
 function JFPCharacter () {
 
-	// instance vars
+  // instance vars
 
-	this.name = "New Guy";
-	this.health = 0;
-	this.shield = 0;
+  this.name = "New Guy";
+  this.health = 0;
+  this.shield = 0;
 
-	this.weapon = {
-		"name"   : "Power Packed Punch",
-		"damage" : 75
-	};
+  this.weapon = {
+    "name"   : "Power Packed Punch",
+    "damage" : 75
+  };
 
-	this.stance = PLAYER_STANCES.ready;
+  this.stance = PLAYER_STANCES.ready;
 
-	this.target;
+  this.target;
 }
 
 JFPCharacter.prototype.isDead = function () {
-	return this.health < 1;
+  return this.health < 1;
 }
 
 JFPCharacter.prototype.isShieldDown = function () {
-	return this.shield < 1;
+  return this.shield < 1;
 }
 
 JFPCharacter.prototype.hitTarget = function (weapon) {
-	if (!this.target || this.target.isDead() || this.isDead()) {
-	// there must be a target
+  if (!this.target || this.target.isDead() || this.isDead()) {
+  // there must be a target
         // the dead can not be attacked
         // the dead can not attack
         console.log("Can't hit because " + this.name + " has no target, or its target is dead, or it's dead!");
-		return;
-	}
+    return;
+  }
 
-	console.log("* " + this.name + " hits " + this.target.name + " with " + this.weapon.name + " *");
+  console.log("* " + this.name + " hits " + this.target.name + " with " + this.weapon.name + " *");
 
-	var hitPoints = this.weapon.damage + Math.floor((Math.random() * 10) + 1);
-	console.log("    Damage Amount: " + hitPoints);
+  var hitPoints = this.weapon.damage + Math.floor((Math.random() * 10) + 1);
+  console.log("    Damage Amount: " + hitPoints);
 
-	switch (this.target.stance) {
-		case PLAYER_STANCES.ready:
-		case PLAYER_STANCES.attacking:
-			this.target.health -= hitPoints;
-			break;
-		case PLAYER_STANCES.defending:
-			if (this.target.isShieldDown()) {
-				this.target.health -= hitPoints;
-			} else {
-				this.target.shield -= hitPoints;
-			}
-	}
+  switch (this.target.stance) {
+    case PLAYER_STANCES.ready:
+    case PLAYER_STANCES.attacking:
+      this.target.health -= hitPoints;
+      break;
+    case PLAYER_STANCES.defending:
+      if (this.target.isShieldDown()) {
+        this.target.health -= hitPoints;
+      } else {
+        this.target.shield -= hitPoints;
+      }
+  }
 
-	if (this.target.health < 0) {
-		this.target.health = 0;
-	}
+  if (this.target.health < 0) {
+    this.target.health = 0;
+  }
 
-	if (this.target.shield < 0) {
-		this.target.shield = 0;
-	}
+  if (this.target.shield < 0) {
+    this.target.shield = 0;
+  }
 
-	console.log("    Results of attack:");
-	console.log("    Target " + this.target.name + " shield: " + this.target.shield + " health: " + this.target.health);
-	if (this.target.isDead()) {
-		console.log("    Target " + this.target.name + " is dead! ");
-	}
+  console.log("    Results of attack:");
+  console.log("    Target " + this.target.name + " shield: " + this.target.shield + " health: " + this.target.health);
+  if (this.target.isDead()) {
+    console.log("    Target " + this.target.name + " is dead! ");
+  }
 }
 
 // test Character Class
@@ -118,16 +118,16 @@ var gameOver = false;
 var gameStartTime = Date.now();
 console.log("Let the game begin!");
 while (!gameOver) {
-	count++;
-	console.log("** Round " + count + " **");
-	morgan.hitTarget();
-	fing.hitTarget();
-	if (morgan.isDead() || fing.isDead()) {
-		gameOver = true;
-		console.log("Game over man!");
-		var gameEndTime = Date.now();
-		var gameElaspedTime = gameEndTime - gameStartTime;
-		console.log("Game Duration in milliseconds: " + gameElaspedTime);
-	};
+  count++;
+  console.log("** Round " + count + " **");
+  morgan.hitTarget();
+  fing.hitTarget();
+  if (morgan.isDead() || fing.isDead()) {
+    gameOver = true;
+    console.log("Game over man!");
+    var gameEndTime = Date.now();
+    var gameElaspedTime = gameEndTime - gameStartTime;
+    console.log("Game Duration in milliseconds: " + gameElaspedTime);
+  };
 }
 
